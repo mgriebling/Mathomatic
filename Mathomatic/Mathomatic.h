030d39d1210d7b5f014106207f36b5cd5bad1663
Michael Griebling <michael.griebling@solinst.com>
2015-04-08 10:51:00 -0400

    Initial Commit

---
+++ Mathomatic/Mathomatic.h
@@ -0,0 +1,19 @@
+//
+//  Mathomatic.h
+//  Mathomatic
+//
+//  Created by Michael Griebling on 8Apr2015.
+//  Copyright (c) 2015 Solinst Canada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mathomatic.
+FOUNDATION_EXPORT double MathomaticVersionNumber;
+
+//! Project version string for Mathomatic.
+FOUNDATION_EXPORT const unsigned char MathomaticVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mathomatic/PublicHeader.h>
+
+
